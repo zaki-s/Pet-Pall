5c743ef13764aff0a6017e643141623af2d3b301
zaki-s <zakijames024@gmail.com>
2025-04-23 13:03:58 +0300

    Added a PetPal display

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+import './App.css'; // Import your CSS file if you want to style it
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <h1>PetPal</h1>
+    </div>
+  );
+};
+
+export default App;
