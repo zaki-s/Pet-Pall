3431cd0b255aa0f8cd2efb72ca5efe9d2f574700
Chrismiles <mileschris466@gmail.com>
2025-04-25 10:36:55 +0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,2 @@
-import React from "react";
-import "./App.css";
 
-function App() {
-  return (
-    <div className="app-container">
-      <h1>Welcome to PetPal 🐾</h1>
-      <p>Track your pet’s care routines and special events with ease.</p>
-    </div>
-  );
-}
 
-export default App;
