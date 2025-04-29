453b4525105e93df1261bd0dfbb3d614e7f3909f
Chrismiles <mileschris466@gmail.com>
2025-04-29 14:58:27 +0300

    update on pages

--- src/Backend/models/Event.js
+++ src/Backend/models/Event.js
@@ -4,7 +4,9 @@ import sequelize from '../config/Database.js';
 const Event = sequelize.define('Event', {
   title: DataTypes.STRING,
   description: DataTypes.STRING,
-  date: DataTypes.DATE
+  date: DataTypes.DATE,
+  event_image: DataTypes.STRING, // <-- Add this line
 });
 
 export default Event;
+
